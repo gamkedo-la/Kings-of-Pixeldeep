48bb072bc29cdf3c04be2c3b47cbc33f90bf4f66
Christer Kaitila <breakdance@mcfunkypants.com>
2023-08-21 16:12:51 -0700

    sidebar click event is sent to one button at a time

--- js/ui/sidebar.js
+++ js/ui/sidebar.js
@@ -43,7 +43,11 @@ function handleSidebarButtonClick(mousePos) {
     for(let i=0;i<currentSidebarButtons.length; i++) {
 	let currentButton = currentSidebarButtons[i];
 	if(isClickOnButton(mousePos, currentButton)) {
-	    currentButton.onClick();
+	    // FIXME: this seems to check ALL buttons but
+        // we should only check visible buttons?
+        console.log("handleSidebarButtonClick is clicking "+currentButton.label);
+        currentButton.onClick(); 
+        break; // stop looking for more buttons, only click the first match
 	}
     }
 }
